5854052cc79f4cf01a41ce22631c8e2f70b71d91
Christian <christianfonseca991@gmail.com>
2023-07-14 13:41:20 -0300

    feat: uma nova navbar, implementação  do js no site

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+const element = document.querySelector(".title")
+const header = document.querySelector("header")
+
+function activeHeaderScroll(){
+    header.classList.toggle('activeHeader', scrollY > 0);
+}
+
+window.addEventListener('scroll', activeHeaderScroll);
+
+function typeWriter(element){
+    const textArray = element.innerHTML.split('')
+    element.innerHTML = ''
+    setTimeout(function(){
+        textArray.forEach((letra, i) => { //Para cada elemento um index
+            setTimeout(function(){
+                element.innerHTML += letra
+            }, 75 * i)
+        })
+    }, 2000)
+}
+
+typeWriter(element);
